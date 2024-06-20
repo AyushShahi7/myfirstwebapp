4ca442d14ecc8111ec6dd61108287cac3617f8dd
Ayush <ayushshai123@gmail.com>
2024-06-20 23:25:44 +0530

    Implemented update todo - 1!

--- src/main/java/com/i2m/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/i2m/springboot/myfirstwebapp/todo/TodoController.java
@@ -37,7 +37,7 @@ public class TodoController {
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String addNewTodoPage(ModelMap model, @Valid Todo todo, BindingResult result) {
+    public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
         if(result.hasErrors()){
             return "todo";
         }
@@ -49,6 +49,24 @@ public class TodoController {
     @RequestMapping("delete-todo")
     public String deleteTodo(@RequestParam int id) {
         todoService.deleteById(id);
+        return "redirect:list-todos";
+    }
+
+    @RequestMapping(value = "update-todo", method = RequestMethod.GET)
+    public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
+        Todo todo = todoService.findById(id);
+        model.addAttribute("todo", todo);
+        return "todo";
+    }
+
+    @RequestMapping(value = "update-todo", method = RequestMethod.POST)
+    public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+        if(result.hasErrors()){
+            return "todo";
+        }
+        String username = (String)model.get("name");
+        todo.setUsername(username);
+        todoService.updateTodo(todo);
         return "redirect:list-todos";
     }
 }
--- src/main/java/com/i2m/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/i2m/springboot/myfirstwebapp/todo/TodoService.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -33,7 +34,19 @@ public class TodoService {
     }
 
     public void deleteById(int id){
-        Predicate<? super Todo> Predicate = todo -> todo.getId() == id;
-        todos.removeIf(Predicate);
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
+    }
+
+
+    public Todo findById(int id) {
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        Todo todo = todos.stream().filter(predicate).findFirst().get();
+        return todo;
+    }
+
+    public void updateTodo(@Valid Todo todo) {
+        deleteById(todo.getId());
+        todos.add(todo);
     }
 }
